b2b124f094b720c68716a92e88773d46e44762e3
Luis-Yael <32848644+Luis-Yael@users.noreply.github.com>
2017-11-23 13:19:54 -0600

    Vistas 2

--- views/entrar_o_registrar.php
+++ views/entrar_o_registrar.php
@@ -6,31 +6,28 @@
 
         <main class="col-12 col-md-10 offset-md-1  col-lg-8 offset-lg-2 contenido-principal">
 
-            <form  class="p-5 mt-5 formulario-contacto" action="<?php echo base_url();?>index.php/welcome/validaLogin" method="POST">
-              
-                    <h1>Bienvenido</h1>
-                  <br><br>
 
-                    <label for="nombre">Usuario</label>
-                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Tu Nombre" >
-  
-         
-          
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="password" >
-                  
-             
-       <br><br>
- 
-                    <a href="<?php echo base_url();?>index.php/welcome/enlace_registro">
+        <form action="<?php echo base_url();?>index.php/Welcome/validaLogin" method="POST">
+              <div class="form-group">
+                <label for="login">Email address:</label>
+                <input type="text" class="form-control" id="login" name="login">
+              </div>
+              <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" name="pwd">
+              </div>
+              <div class="form-check">
+            
+              </div>
+
+                  <a href="<?php echo base_url();?>index.php/welcome/enlace_registro">
                             <button type="button" class="btn btn-success btn-lg" style="margin: 0 11.1%;" >    Registrar</button>
                     </a>
 
-                   
-                  <button type="submit" class="btn btn-default btn-success btn-lg" style="margin: 0 11.1%;margin-top: 0px;">Iniciar sesion</button>
-                  
-          
-            </form>
+              <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+              
+</form>
+
         </main>
    
     </div>
